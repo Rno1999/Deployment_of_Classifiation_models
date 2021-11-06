46af75e6d51fcfbf9c1e61276a7a6c4a178e7340
Rno1999 <93844836+Rno1999@users.noreply.github.com>
2021-11-07 02:05:24 +0300

    Add files via upload

--- HModel.py
+++ HModel.py
@@ -1,98 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[32]:
+# In[57]:
 
 
 #For data manipulation
 import pandas as pd
 import numpy as np
 
-#For plot
-get_ipython().run_line_magic('matplotlib', 'inline')
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.figure_factory as ff
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 
 #For modeling
 from sklearn.model_selection import train_test_split
-from sklearn import metrics
-from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.naive_bayes import GaussianNB
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
-from xgboost import XGBClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import ExtraTreesClassifier
-import plotly.express as px
 from sklearn.metrics import precision_recall_fscore_support
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import roc_auc_score, roc_curve
-from sklearn.ensemble import StackingClassifier
-from mlxtend.classifier import StackingClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import GridSearchCV
-from sklearn import linear_model, decomposition, datasets
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV, cross_val_score
-from sklearn.preprocessing import StandardScaler
-from sklearn import tree
-
-
-#For store dataset into database
-from sqlalchemy import *
-
-#For inserting images
-from IPython.display import Image
-
-#For warning
-import warnings
-warnings.filterwarnings("ignore")
-
 import pickle
 
 
-# In[8]:
+# In[38]:
 
 
 df=pd.read_csv('heart_Disease_Dataset.csv')
 
 
-# In[9]:
-
-
-engine=create_engine('sqlite:///HD.db')
-df.to_sql('HD_data',engine,if_exists='replace',index=False)
-
-
-# In[10]:
-
-
-#To check if the HD_data table existing
-tables=engine.table_names()
-print(tables);
-
-
-# In[11]:
-
-
-df=pd.read_sql('select * from HD_data',engine)
-
-
-# In[12]:
+# In[39]:
 
 
 df.head()
 
 
-# In[13]:
+# In[40]:
 
 
 categorical_val = []
@@ -106,16 +43,7 @@ for column in df.columns:
         continous_val.append(column)
 
 
-# In[16]:
-
-
-fig = plt.figure(figsize=(20, 20))
-ax = fig.gca()
-df.plot(kind='box', subplots=True, layout=(4, 4), sharex=False, ax=ax)
-plt.show()
-
-
-# In[17]:
+# In[41]:
 
 
 def handle_outlier_IQR(df):
@@ -127,7 +55,7 @@ def handle_outlier_IQR(df):
     return df_new
 
 
-# In[18]:
+# In[42]:
 
 
 df["trestbps"]=handle_outlier_IQR(df['trestbps'])
@@ -136,30 +64,21 @@ df["thalach"]=handle_outlier_IQR(df['thalach'])
 df["oldpeak"]=handle_outlier_IQR(df['oldpeak'])
 
 
-# In[20]:
-
-
-fig = plt.figure(figsize=(20, 20))
-ax = fig.gca()
-df.plot(kind='box', subplots=True, layout=(4, 4), sharex=False, ax=ax)
-plt.show()
-
-
-# In[21]:
+# In[43]:
 
 
 y = df.target.values
 x_data =df[continous_val]
 
 
-# In[22]:
+# In[44]:
 
 
 categorical_val.remove('target')
 dataset = pd.get_dummies(df, columns = categorical_val)
 
 
-# In[23]:
+# In[45]:
 
 
 rest=dataset.drop(['age','trestbps','chol','thalach','oldpeak','target'], axis='columns')
@@ -169,13 +88,13 @@ frames = [rest, x]
 x = pd.concat(frames, axis = 1)
 
 
-# In[24]:
+# In[46]:
 
 
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.4,random_state=0)
 
 
-# In[25]:
+# In[47]:
 
 
 #transpose matrices
@@ -185,7 +104,7 @@ x_test = x_test.T
 y_test = y_test.T
 
 
-# In[26]:
+# In[48]:
 
 
 accuracies = {}
@@ -195,7 +114,7 @@ recalls ={}
 
 # ### Creating Model for K-Nearest Neighbour (KNN)
 
-# In[27]:
+# In[56]:
 
 
 # try to find best k value
@@ -205,18 +124,12 @@ for i in range(1,20):
     knn2.fit(x_train.T, y_train.T)
     scoreList.append(knn2.score(x_test.T, y_test.T))
     
-plt.plot(range(1,20), scoreList)
-plt.xticks(np.arange(1,20,1))
-plt.xlabel("K value")
-plt.ylabel("Score")
-plt.show()
-
 acc = max(scoreList)*100
 accuracies['KNN'] = np.round(acc,2)
 print("Maximum KNN Score is {:.2f}%".format(acc))
 
 
-# In[28]:
+# In[50]:
 
 
 knn = KNeighborsClassifier(n_neighbors = 1)  # n_neighbors means k
@@ -225,7 +138,7 @@ prediction = knn.predict(x_test.T)
 print("{} NN Score: {:.2f}%".format(2, knn.score(x_test.T, y_test.T)*100))
 
 
-# In[29]:
+# In[51]:
 
 
 pred = knn.predict(x_test.T)
@@ -234,21 +147,21 @@ precisions['KNN'] = np.round(precision,2)
 recalls['KNN'] = np.round(recall,2)
 
 
-# In[30]:
+# In[52]:
 
 
 print(confusion_matrix(y_test.T, pred))
 print(classification_report(y_test.T, pred))
 
 
-# In[33]:
+# In[53]:
 
 
 with open('Heart_Model.pkl', 'wb') as file:
   pickle.dump(knn, file)
 
 
-# In[36]:
+# In[55]:
 
 
 model=pickle.load(open('Heart_Model.pkl','rb'))
